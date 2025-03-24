45e7fab3033a21e568e4617bde76c8ebc635533d
byunghoon <astraljoker@gmail.com>
2025-03-24 01:48:17 -0400

    chore: update site URL

--- frontend/src/constant/config.ts
+++ frontend/src/constant/config.ts
@@ -1,6 +1,5 @@
 export const siteConfig = {
   title: 'Solamon',
   description: 'Solamon',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://solana-monster.vercel.app',
 }
