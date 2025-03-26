19d80d518d0b86f0cd6b70fd8a2f21be69643cb6
byunghoon <astraljoker@gmail.com>
2025-03-26 05:11:02 -0400

    feat: add attackType to battle actions in game replay

--- frontend/src/game/data/replay.ts
+++ frontend/src/game/data/replay.ts
@@ -1,10 +1,13 @@
 import { CardData } from './card'
 
+export type AttackType = 'NONE' | 'CRITICAL' | 'HALVED'
+
 export interface BattleAction {
   isPlayer: boolean
   atkIdx: number
   defIdx: number
   damage: number
+  attackType: AttackType
 }
 
 export interface BattleReplay {
