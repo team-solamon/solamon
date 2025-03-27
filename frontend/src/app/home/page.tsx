1ce5b6ffb7ae11b60b75288159e0ef8badd611f0
ByungHoon Kang <astraljoker@gmail.com>
2025-03-27 18:39:52 -0400

    feat: adjust grid layout

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -170,7 +170,7 @@ const HomePage = () => {
                 )
             )}
           </div>
-          <div className='card-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4'>
+          <div className='card-list grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4'>
             {myCards.map((card, index) => (
               <div
                 key={index}
