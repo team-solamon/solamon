19f7323324e900551ad1e158ba6520b1332057e0
pluto <pluto@ao.xyz>
2025-03-28 20:38:22 +0900

    feat: create account and request airdrop

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,51 @@
 'use client'
 
+import { clusterApiUrl, Connection, Keypair } from '@solana/web3.js'
 import { useRouter } from 'next/navigation'
 import * as React from 'react'
+import { useState } from 'react'
+
+import {
+  getFromLocalStorage,
+  setKeypairToLocalStorage,
+  getKeypairFromLocalStorage,
+  getConnection,
+} from '@/lib/helper'
+
 import Button from '@/components/Button'
 
 export default function HomePage() {
   const router = useRouter()
 
-  const handleCreateAccount = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const pk = getKeypairFromLocalStorage()
+  const handleCreateAccount = async () => {
+    if (!pk) {
+      setIsLoading(true)
+      // generate random account
+      setKeypairToLocalStorage()
+
+      const keypair = getKeypairFromLocalStorage()
+      // Create a connection to the Solana devnet
+      const connection = getConnection()
+
+      if (!keypair) {
+        console.error('Failed to create keypair')
+        return
+      }
+
+      // Request airdrop
+      try {
+        const airdropSignature = await connection.requestAirdrop(
+          keypair.publicKey,
+          1e9 // 1 SOL in lamports
+        )
+        await connection.confirmTransaction(airdropSignature)
+      } catch (error) {
+        console.error('Airdrop failed', error)
+      }
+      setIsLoading(false)
+    }
     router.push('/home')
   }
 
@@ -21,7 +59,9 @@ export default function HomePage() {
           className='w-full max-w-2xl rounded-lg'
         />
         <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2'>
-          <Button onClick={handleCreateAccount}>Create account</Button>
+          <Button onClick={handleCreateAccount} disabled={isLoading}>
+            {isLoading ? 'Creating...' : pk ? 'Start' : 'Create account'}
+          </Button>
         </div>
       </div>
     </main>
--- frontend/src/lib/helper.ts
+++ frontend/src/lib/helper.ts
@@ -1,3 +1,5 @@
+import { clusterApiUrl, Connection, Keypair } from '@solana/web3.js'
+
 export function getFromLocalStorage(key: string): string | null {
   if (typeof window !== 'undefined') {
     return window.localStorage.getItem(key)
@@ -10,4 +12,39 @@ export function getFromSessionStorage(key: string): string | null {
     return sessionStorage.getItem(key)
   }
   return null
+}
+
+export function setToLocalStorage(key: string, value: string) {
+  if (typeof window !== 'undefined') {
+    window.localStorage.setItem(key, value)
+  }
+}
+
+export function setToSessionStorage(key: string, value: string) {
+  if (typeof sessionStorage !== 'undefined') {
+    sessionStorage.setItem(key, value)
+  }
+}
+
+export function setKeypairToLocalStorage() {
+  const account = Keypair.generate()
+  setToLocalStorage('pk', account.secretKey.toString())
+}
+
+export function getKeypairFromLocalStorage(): Keypair | null {
+  const pk = getFromLocalStorage('pk')
+  if (!pk) return null
+
+  try {
+    // Convert the string back to a Uint8Array properly
+    const privateKeyArray = Uint8Array.from(pk.split(',').map(Number))
+    return Keypair.fromSecretKey(privateKeyArray)
+  } catch (error) {
+    console.error('Error creating keypair from stored data:', error)
+    return null
+  }
+}
+
+export const getConnection = () => {
+  return new Connection(clusterApiUrl('devnet'), 'confirmed')
 }
