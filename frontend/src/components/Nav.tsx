adf12bd44250d491c66ae95f2a556aa6f0c941a4
byunghoon <astraljoker@gmail.com>
2025-03-29 05:16:35 -0400

    feat: add solamon-green color

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -26,16 +26,16 @@ const Nav: React.FC<NavProps> = ({ onNewCard, onOpenTutorial }) => {
     <header className='header flex justify-between items-center mb-6'>
       <span
         onClick={onOpenTutorial}
-        className='text-[#9FF500] cursor-pointer font-semibold'
+        className='text-solamon-green cursor-pointer font-semibold'
       >
         Tutorial
       </span>
       <h1 className='text-4xl font-bold'>SOLAMON</h1>
       <div className='wallet-info text-right'>
-        <span className='block text-sm text-[#9FF500]'>
+        <span className='block text-sm text-solamon-green'>
           {keypair?.publicKey.toBase58()}
         </span>
-        <span className='block text-lg text-[#9FF500]'>
+        <span className='block text-lg text-solamon-green'>
           {balance ? balance : '0.00'}
         </span>
       </div>
