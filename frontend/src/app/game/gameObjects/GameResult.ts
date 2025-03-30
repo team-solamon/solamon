12ef3353aa267b64fd8788bfbe5b8e218d29b48f
byunghoon <astraljoker@gmail.com>
2025-03-29 23:42:15 -0400

    feat: enhance particle effects for victory and defeat

--- frontend/src/app/game/gameObjects/GameResult.ts
+++ frontend/src/app/game/gameObjects/GameResult.ts
@@ -62,16 +62,12 @@ export class GameResult extends Phaser.GameObjects.Container {
       ease: 'Back.easeOut',
     })
 
-    this.scene.time.delayedCall(5000, () => {
-      this.particles.forEach((p) => p.destroy())
-    })
-
     return resultMessage
   }
 
   private createVictoryEffects() {
     this.particles = [
-      this.scene.add.particles(400, 0, 'spark', {
+      this.scene.add.particles(0, 0, 'spark', {
         speed: { min: 100, max: 300 },
         angle: { min: 80, max: 100 },
         scale: { start: 0.4, end: 0 },
@@ -80,8 +76,15 @@ export class GameResult extends Phaser.GameObjects.Container {
         quantity: 1,
         frequency: 50,
         tint: [0xffd700, 0xffa500, 0xffff00],
+        emitZone: {
+          type: 'random',
+          source: new Phaser.Geom.Rectangle(0, 0, 800, 10),
+          quantity: 1,
+        },
+        x: 0,
+        y: 0,
       }),
-      this.scene.add.particles(400, 300, 'spark', {
+      this.scene.add.particles(0, 0, 'spark', {
         speed: { min: 50, max: 150 },
         scale: { start: 0.1, end: 2 },
         alpha: { start: 0.6, end: 0 },
@@ -90,28 +93,62 @@ export class GameResult extends Phaser.GameObjects.Container {
         frequency: 100,
         tint: 0xffd700,
         blendMode: 'ADD',
+        emitZone: {
+          type: 'random',
+          source: new Phaser.Geom.Circle(400, 300, 150),
+          quantity: 1,
+        },
       }),
     ]
   }
 
   private createDefeatEffects() {
     this.particles = [
-      this.scene.add.particles(400, 300, 'spark', {
-        speed: { min: 50, max: 100 },
-        scale: { start: 1, end: 0 },
-        alpha: { start: 0.5, end: 0 },
-        lifespan: 2000,
-        frequency: 50,
-        tint: 0x333333,
+      this.scene.add.particles(0, 0, 'spark', {
+        speed: { min: 50, max: 150 },
+        angle: { min: 230, max: 310 },
+        scale: { start: 0.6, end: 0 },
+        alpha: { start: 0.8, end: 0 },
+        gravityY: 100,
+        lifespan: 3000,
+        quantity: 2,
+        frequency: 40,
+        tint: [0x990000, 0x660000, 0x330000],
+        emitZone: {
+          type: 'random',
+          source: new Phaser.Geom.Rectangle(0, 0, 800, 100),
+          quantity: 1,
+        },
       }),
-      this.scene.add.particles(400, 300, 'spark', {
-        speed: { min: 100, max: 200 },
-        scale: { start: 0.2, end: 0 },
+      this.scene.add.particles(0, 0, 'spark', {
+        speed: { min: 100, max: 300 },
+        scale: { start: 0.4, end: 0 },
         angle: { min: 0, max: 360 },
-        lifespan: 1000,
-        frequency: 100,
-        tint: 0xff0000,
+        lifespan: 1500,
+        frequency: 80,
+        quantity: 3,
+        tint: [0xff0000, 0xff3333, 0xff6666],
         blendMode: 'ADD',
+        emitZone: {
+          type: 'random',
+          source: new Phaser.Geom.Circle(400, 300, 100),
+          quantity: 1,
+        },
+      }),
+      this.scene.add.particles(0, 0, 'spark', {
+        speed: { min: 20, max: 70 },
+        scale: { start: 1, end: 0 },
+        alpha: { start: 0.3, end: 0 },
+        angle: { min: 230, max: 310 },
+        rotate: { min: 0, max: 360 },
+        lifespan: 4000,
+        frequency: 70,
+        tint: 0x222222,
+        emitZone: {
+          type: 'random',
+          source: new Phaser.Geom.Circle(400, 300, 120),
+          quantity: 1,
+        },
       }),
     ]
   }
