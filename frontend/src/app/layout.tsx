aa598bce2ec4efaa0e4aaaf42b2380cb1229a1a8
pluto <pluto@ao.xyz>
2025-03-31 20:29:35 +0900

    feat: add vibe jam 2025 tag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -57,6 +57,29 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <a
+        target='_blank'
+        href='https://jam.pieter.com'
+        style={{
+          fontFamily: 'system-ui, sans-serif',
+          position: 'fixed',
+          bottom: '-1px',
+          right: '-1px',
+          padding: '7px',
+          fontSize: '14px',
+          fontWeight: 'bold',
+          background: '#fff',
+          color: '#000',
+          textDecoration: 'none',
+          zIndex: '10',
+          borderTopLeftRadius: '12px',
+          zIndex: '10000',
+          border: '1px solid #fff',
+        }}
+      >
+        🕹️ Vibe Jam 2025
+      </a>
+
       <body>
         <LoadingProvider>
           <BalanceProvider>
