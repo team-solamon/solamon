5069e091efa0923ed4f1f67f0ae8aa2316cce463
byunghoon <astraljoker@gmail.com>
2025-03-31 05:07:00 -0400

    feat: enhance CardGuideModal with species probabilities

--- frontend/src/components/modals/CardGuideModal.tsx
+++ frontend/src/components/modals/CardGuideModal.tsx
@@ -1,9 +1,14 @@
 'use client'
 
 import React from 'react'
-import dynamic from 'next/dynamic'
+
 import Modal from '../Modal'
 import { useModal } from '@/contexts/ModalContext'
+import Button from '../Button'
+import { stringToElement } from '@/lib/solana-helper'
+import Card from '../Card'
+import CardElementProbabilities from '../CardElementProbabilities'
+import { CARD_SPECIES_PROBABILITIES } from '@/constant/env'
 
 interface TutorialModalProps {
   onNewCard: () => void
@@ -20,11 +25,25 @@ const CardGuideModal: React.FC<TutorialModalProps> = ({
     <Modal
       isOpen={modals['guide']}
       onClose={() => closeModal('guide')}
-      title='🃏 Card Guide!!!'
+      title='🃏 Card Guide!'
       maxWidth='600px'
     >
-      <div className='flex flex-col items-center justify-center'>
-        🃏 Card Guide!!! contents
+      <div className='flex flex-col h-full'>
+        <div className='grid grid-cols-5 gap-4 p-2'>
+          {CARD_SPECIES_PROBABILITIES.map((card, index) => (
+            <div
+              key={index}
+              className='card bg-gray-700 p-2 rounded-lg flex-shrink-0 relative'
+            >
+              <Card species={card.species} className='mx-auto' />
+              <CardElementProbabilities data={card.probabilities} />
+            </div>
+          ))}
+        </div>
+        <div className='mt-4 flex justify-around sticky bottom-0 bg-[#978578] py-3'>
+          <Button onClick={onNewCard}>New Card</Button>
+          <Button onClick={onOpenBattle}>Open Battle</Button>
+        </div>
       </div>
     </Modal>
   )
--- frontend/src/constant/env.ts
+++ frontend/src/constant/env.ts
@@ -1,3 +1,5 @@
+import { stringToElement } from '@/lib/solana-helper'
+
 export const isProd = process.env.NODE_ENV === 'production'
 export const isLocal = process.env.NODE_ENV === 'development'
 
@@ -9,3 +11,91 @@ export const NEW_CARD_SOL_PRICE = 0.1
 export const FIGHT_SOL_PRICE = 0.1
 
 export const WINNER_SOL_REWARD = 0.2
+
+interface CardWithProbabilities {
+  species: number
+  probabilities: { element: Element; probability: number }[]
+}
+
+export const CARD_SPECIES_PROBABILITIES: CardWithProbabilities[] = [
+  {
+    species: 0,
+    probabilities: [
+      { element: stringToElement('fire'), probability: 80 },
+      { element: stringToElement('water'), probability: 10 },
+      { element: stringToElement('wood'), probability: 10 },
+    ],
+  },
+  {
+    species: 1,
+    probabilities: [
+      { element: stringToElement('fire'), probability: 80 },
+      { element: stringToElement('earth'), probability: 10 },
+      { element: stringToElement('metal'), probability: 10 },
+    ],
+  },
+  {
+    species: 2,
+    probabilities: [
+      { element: stringToElement('water'), probability: 80 },
+      { element: stringToElement('fire'), probability: 10 },
+      { element: stringToElement('metal'), probability: 10 },
+    ],
+  },
+  {
+    species: 3,
+    probabilities: [
+      { element: stringToElement('water'), probability: 80 },
+      { element: stringToElement('wood'), probability: 10 },
+      { element: stringToElement('earth'), probability: 10 },
+    ],
+  },
+  {
+    species: 4,
+    probabilities: [
+      { element: stringToElement('wood'), probability: 80 },
+      { element: stringToElement('water'), probability: 10 },
+      { element: stringToElement('fire'), probability: 10 },
+    ],
+  },
+  {
+    species: 5,
+    probabilities: [
+      { element: stringToElement('wood'), probability: 80 },
+      { element: stringToElement('earth'), probability: 10 },
+      { element: stringToElement('metal'), probability: 10 },
+    ],
+  },
+  {
+    species: 6,
+    probabilities: [
+      { element: stringToElement('earth'), probability: 80 },
+      { element: stringToElement('fire'), probability: 10 },
+      { element: stringToElement('metal'), probability: 10 },
+    ],
+  },
+  {
+    species: 7,
+    probabilities: [
+      { element: stringToElement('earth'), probability: 80 },
+      { element: stringToElement('water'), probability: 10 },
+      { element: stringToElement('wood'), probability: 10 },
+    ],
+  },
+  {
+    species: 8,
+    probabilities: [
+      { element: stringToElement('metal'), probability: 80 },
+      { element: stringToElement('fire'), probability: 10 },
+      { element: stringToElement('earth'), probability: 10 },
+    ],
+  },
+  {
+    species: 9,
+    probabilities: [
+      { element: stringToElement('metal'), probability: 80 },
+      { element: stringToElement('water'), probability: 10 },
+      { element: stringToElement('wood'), probability: 10 },
+    ],
+  },
+]
